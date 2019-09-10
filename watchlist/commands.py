939244caa7254f310db00c4f2222578da03b18e5
drunkwretch <drunkwretch@gmail.com>
2019-09-11 01:40:59 +0800

    Origin application with package

--- watchlist/commands.py
+++ watchlist/commands.py
@@ -1,6 +1,7 @@
 import click
-import app
-from app import db, User, Movie
+
+from watchlist import app, db
+from watchlist.models import User, Movie
 
 
 @app.cli.command()
@@ -37,6 +38,28 @@ def forge():
     for m in movies:
         movie = Movie(title=m['title'], year=m['year'])
         db.session.add(movie)
+
+    db.session.commit()
+    click.echo('Done.')
+
+
+@app.cli.command()
+@click.option('--username', prompt=True, help='The username used to login.')
+@click.option('--password', prompt=True, hide_input=True, confirmation_prompt=True, help='The password used to login.')
+def admin(username, password):
+    """Create user"""
+    db.create_all()
+
+    user = User.query.first()
+    if user is not None:
+        click.echo('Updating user...')
+        user.username = username
+        user.set_password(password)
+    else:
+        click.echo('Creating user...')
+        user = User(username=username, name='Admin')
+        user.set_password(password)
+        db.session.add(user)
 
     db.session.commit()
     click.echo('Done.')
