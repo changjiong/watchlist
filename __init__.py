c870050d6ef918aeb2a1502ae1ca7f281b06b0c8
drunkwretch <drunkwretch@gmail.com>
2019-09-11 14:47:18 +0800

    User authentication with Flask-Login

--- __init__.py
+++ __init__.py
@@ -5,7 +5,6 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
-from watchlist.models import User
 
 WIN = sys.platform.startswith('win')
 if WIN:
@@ -15,21 +14,24 @@ else:
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'dev')
-app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(os.path.abspath(app.root_path), os.getenv('DATABASE_FILE', 'data.db'))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
 db = SQLAlchemy(app)
 login_manager = LoginManager(app)
-login_manager.login_view = 'login'
 
 @login_manager.user_loader
 def load_user(user_id):
+    from watchlist.models import User
     user = User.query.get(int(user_id))
     return user
 
+login_manager.login_view = 'login'
+
 @app.context_processor
 def inject_user():
+    from watchlist.models import User
     user = User.query.first()
     return dict(user=user)
 
--- views.py
+++ views.py
@@ -4,16 +4,20 @@ from flask_login import login_user, logout_user, login_required, current_user
 from watchlist import app, db
 from watchlist.models import User, Movie
 
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         if not current_user.is_authenticated:
             return redirect(url_for('index'))
-        title = request.form.get('title')
-        year = request.form.get('year')
+
+        title = request.form['title']
+        year = request.form['year']
+
         if not title or not year or len(year) > 4 or len(title) > 60:
             flash('Invalid input')
             return redirect(url_for('index'))
+
         movie = Movie(title=title, year=year)
         db.session.add(movie)
         db.session.commit()
@@ -30,12 +34,12 @@ def edit(movie_id):
     movie = Movie.query.get_or_404(movie_id)
 
     if request.method == 'POST':
-        title = request.form.get('title')
-        year = request.form.get('year')
+        title = request.form['title']
+        year = request.form['year']
 
         if not title or not year or len(year) > 4 or len(title) > 60:
-            flash('Invalid input')
-            return redirect(url_for('index'))
+            flash('Invalid input.')
+            return redirect(url_for('edit', movie_id=movie_id))
 
         movie.title = title
         movie.year = year
@@ -46,6 +50,7 @@ def edit(movie_id):
     return render_template('edit.html', movie=movie)
 
 
+
 @app.route('/movie/delete/<int:movie_id>', methods=['POST'])
 @login_required
 def delete(movie_id):
@@ -53,7 +58,26 @@ def delete(movie_id):
     db.session.delete(movie)
     db.session.commit()
     flash('Item deleted.')
-    return render_template(url_for('index'))
+    return redirect(url_for('index'))
+
+
+@app.route('/settings', methods=['GET', 'POST'])
+@login_required
+def settings():
+    if request.method == 'POST':
+        name = request.form['name']
+
+        if not name or len(name) > 20:
+            flash('Invalid input.')
+            return redirect(url_for('settings'))
+
+        user = User.query.first()
+        user.name = name
+        db.session.commit()
+        flash('Settings updated.')
+        return redirect(url_for('index'))
+
+    return render_template('settings.html')
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -86,20 +110,4 @@ def logout():
     flash('Goodbye.')
     return redirect(url_for('index'))
 
-
-@app.route('/settings', methods=['GET', 'POST'])
-@login_required
-def settings():
-    if request.method == 'POST':
-        name = request.form['name']
-        if not name or len(name) > 20:
-            flash('Invalid input.')
-            return redirect(url_for('settings'))
-
-        current_user.name = name
-        db.session.commit()
-        flash('Settings updated.')
-        return redirect(url_for('index'))
-
-    return render_template('settings.html')
 
